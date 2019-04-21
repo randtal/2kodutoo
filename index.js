0349d1b7059100caeed7a3a3cb073d45f1c09e60
Johanna Maarja Eller <43164763+jmaarjae@users.noreply.github.com>
2019-04-21 22:18:06 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,101 +1,109 @@
 /*jshint esversion:6*/
-//console.log("Javascripti fail أµigesti أ¼hendatud");
-
 class Entry{
-  constructor(title, description, toDoDate){
+  constructor(title, description, date){
     this.title = title;
     this.description = description;
-    this.date = toDoDate;
+    this.date = date;
     this.done = false;
   }
 }
 
 class ToDo{
+
   constructor(){
     this.entries = JSON.parse(window.localStorage.getItem('entries')) || [];
-    document.querySelector("#addButton").addEventListener('click', () => this.addEntry());
+
+    document.querySelector('#addButton').addEventListener('click', () => this.addEntry());
     document.querySelector("#saveButton").addEventListener('click', () => this.saveToFile());
-    this.render(this.entries);
+   
+
+    this.render();
   }
 
-  render(entries){
-    console.log("entries",entries)
+  render(){
     if(document.querySelector('.todo-list')){
       document.body.removeChild(document.querySelector('.todo-list'));
     }
-    const ul = document.createElement('ul');
-    ul.className = "todo-list";
 
-    entries.forEach((entry, entryIndex)=>{
+    const ul = document.createElement('ul');
+    ul.className = 'todo-list';
+
+    this.entries.forEach((entry, entryIndex)=>{
       const li = document.createElement('li');
-      const removeTaskButton = document.createElement('button');
+      const removeTaskButton = document.createElement('div');
       const removeIcon = document.createTextNode('\u00D7');
 
       li.classList.add('entry');
       removeTaskButton.className = "delete-task-button";
 
-      li.addEventListener('click', (event) =>{
-        event.target.classList.add('task--completed');
+      li.addEventListener('click', (event)=>{
+        event.target.classList.add('task-completed');
 
         if(entry.done){
           entry.done = false;
-
-          this.entries = this.entries.slice(0, entryIndex).concat(this.entries.slice(entryIndex+1, this.entries.length));
-
-          this.entries.unshift(entry);
-
         }else{
           entry.done = true;
-
-          this.entries = this.entries.slice(0, entryIndex).concat(this.entries.slice(entryIndex+1, this.entries.length));
-
-          this.entries.push(entry);
-          li.style.textDecoration = "line-through";
-          li.style.backgroundColor = "lightgreen";
-
         }
 
-        //this.saveEntriesInLocalStorage();
-        this.render(this.entries);
-
-
+        this.saveInLocalStorage();
+        this.render();
       });
-
-      if(entry.done){
-        li.style.textDecoration = "line-through";
-        li.style.backgroundColor = "darkRed";
-      }
 
       removeTaskButton.addEventListener('click', ()=>{
         ul.removeChild(li);
-        this.entries = this.entries.slice(0, entryIndex).concat(this.entries.slice(entryIndex+1, this.entries.length));
-        console.log("removeTaskButton", this.entries)
-        this.saveEntriesInLocalStorage();
-        this.render(this.entries);
+        this.todos = this.entries.slice(0, entryIndex).concat(this.entries.slice(entryIndex + 1, this.entries.length));
+        this.saveInLocalStorage();
       });
 
+      if(entry.done){
+        li.style.backgroundColor = "lightgreen";
+        li.style.textDecoration = "line-through";
+      }
+
+      let today = new Date();
+      let dd = today.getDate();
+      let mm = today.getMonth() + 1; //January is 0!
+      let yyyy = today.getFullYear();
+
+      if (dd < 10) {
+        dd = '0' + dd;
+      }
+
+      if (mm < 10) {
+        mm = '0' + mm;
+      }
+
+      today = yyyy + '-' + mm + '-' + dd;
+      if(entry.date == today){
+        li.style.border = "2px solid red";
+      }
+
       removeTaskButton.appendChild(removeIcon);
-      li.innerHTML = `${entry.title}<br>${entry.description}<br> ${entry.date}`;
+      li.innerHTML = `${entry.title} <br> ${entry.description} <br> ${entry.date}`;
       li.appendChild(removeTaskButton);
       ul.appendChild(li);
 
     });
 
     document.body.appendChild(ul);
-
   }
 
   addEntry(){
-    const titleValue = document.querySelector("#title").value;
-    const descriptionValue = document.querySelector("#description").value;
+    const titleValue = document.querySelector('#title').value;
+    const descriptionValue = document.querySelector('#description').value;
     const dateValue = document.querySelector('#date').value;
+
     this.entries.push(new Entry(titleValue, descriptionValue, dateValue));
-    this.saveEntriesInLocalStorage();
-    this.render(this.entries);
-    console.log(this.entries);
+
+    this.saveInLocalStorage();
+
+    this.render();
   }
 
+let entries = new todos[];
+
   saveToFile(){
+    console.log("saveis");
     $.post('server.php', {save: todos}).done(function(){
       console.log('done');
     }).fail(function(){
@@ -105,37 +113,26 @@ class ToDo{
     });
   }
 
-  saveEntriesInLocalStorage(){
-    console.log("saveEntries in localStorage", this.entries)
+  saveInLocalStorage(){
     window.localStorage.setItem('entries', JSON.stringify(this.entries));
   }
 }
 
-let todos = [];
 
-// $('#saveButton').on('click', ()=>saveToFile());
 $('#loadButton').on('click', ()=>render());
 
 function render(){
-  $('#todos').html("");
+  $('#entries').html("");
   $.get('database.txt', function(data){
     let content = JSON.parse(data).content;
 
-    content.forEach(function(todo, todoIndex){
-      console.log(todoIndex);
-      $('#todos').append('<ul><li>'+ todo.title+'</li><li>'+ todo.description +'</li><li>'+ todo.date +'</li></ul>');
+    content.forEach(function(entry, entryIndex){
+      console.log(entryIndex);
+      $('#entries').append('<ul><li>'+ entry.title+'</li><li>'+ entry.description +'</li><li>'+ entry.date +'</li></ul>');
     });
   });
 }
 
+const entry = new ToDo();
 
-// function saveToFile(){
-//   $.post('server.php', {save: todos}).done(function(){
-//     console.log('done');
-//   }).fail(function(){
-//     console.log('fail');
-//   }).always(function(){
-//     console.log('always');
-//   });
-// }
-const todo = new ToDo();
+
